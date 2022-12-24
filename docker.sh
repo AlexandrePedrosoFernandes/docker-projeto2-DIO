e45ce9334e598985ad1584365fa907206fe0651e
AlexandrePedrosoFernandes <alexandrepedrosofernandes@gmail.com>
2022-12-24 08:11:15 -0300

    Update docker.sh
    Retificado código docker.sh com  a inclusão do comando para instalação do docker-compose

--- docker.sh
+++ docker.sh
@@ -2,5 +2,6 @@
 curl -fsSL https://get.docker.com | sudo bash
 sudo curl -fsSL "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+sudo apt-get -y docker-compose
 sudo usermod -aG docker vagrant
 sudo docker-compose up -d
